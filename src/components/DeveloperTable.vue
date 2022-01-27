09628887175a09b9e8a51ffc59c3f438f0d4f61f
José Bruno <josebrunocampanholi@gmail.com>
2022-01-26 22:16:22 -0300

    :rocket: filters to search developers.

--- src/components/DeveloperTable.vue
+++ src/components/DeveloperTable.vue
@@ -1,13 +1,38 @@
 <template>
   <section>
-    <table class="table">
+    <div class="text-start row mb-4">
+      <h5>Filtros</h5>
+      <div class="col-md-2">
+        <input type="text" v-model="filters.name" class="form-control" placeholder="Nome...">
+      </div>
+      <div class="col-md-2">
+        <input type="text" v-model="filters.age" class="form-control" placeholder="Idade..." @input="filters.age = onlyNumbers(filters.age)">
+      </div>
+      <div class="col-md-2">
+        <input type="date" v-model="filters.birthdate" class="form-control" placeholder="Data Nascimento">
+      </div>
+      <div class="col-md-2">
+        <select v-model="filters.sex" class="form-select">
+          <option v-for="sex in sexOptions" :key="sex.value" :value="sex.value">{{sex.text}}</option>
+        </select>
+      </div>
+      <div class="col-md-1 d-flex justify-content-around">
+        <button title="Filtrar" class="btn btn-sm btn-secondary" @click="getDevelopersWithFilters">
+          <b-icon icon="search"></b-icon>
+        </button>
+        <button title="Limpar filtro" class="btn btn-sm btn-secondary" @click="cleanFilters">
+          <b-icon icon="trash"></b-icon>
+        </button>
+      </div>
+    </div>
+    <table class="table table-responsive-lg">
       <thead>
       <tr>
         <th>Uuid</th>
         <th>Nome</th>
         <th>Sexo</th>
         <th>Idade</th>
-        <th>Aniversário</th>
+        <th>Data Nascimento</th>
         <th>Ações</th>
       </tr>
       </thead>
@@ -32,6 +57,9 @@
       </tr>
       </tbody>
     </table>
+    <div v-if="!developers.length">
+      <p>Nenhum resultado encontrado.</p>
+    </div>
     <div class="table-footer">
       <div class="">
         <p>Total: {{developers.length}}</p>
@@ -43,12 +71,20 @@
 <script>
 import http from "@/services/http";
 import apiRoutes from "@/helpers/apiRoutes";
+import sexOptions from "@/helpers/sexOptions";
 
 export default {
   name: "DeveloperTable",
   data() {
     return {
       developers: [],
+      filters: {
+        name: null,
+        age: null,
+        sex: null,
+        birthdate: null
+      },
+      sexOptions
     }
   },
   created() {
@@ -60,6 +96,18 @@ export default {
         this.developers = data.content;
       }).catch(err => console.log(err))
 
+    },
+    getDevelopersWithFilters() {
+      http.get(apiRoutes.developers, {
+        params: {
+          name: this.filters.name,
+          sex: this.filters.sex,
+          age: this.filters.age,
+          birthdate: this.filters.birthdate
+        }
+      }).then(({data}) => {
+        this.developers = data.content;
+      }).catch(err => console.log(err))
     },
     confirm(id) {
         if (!confirm('Tem certeza que deseja excluir o registro?')) {
@@ -85,6 +133,15 @@ export default {
 
     goToReadyOnly(id) {
       return this.$emit('isReadyOnly', id);
+    },
+
+    cleanFilters() {
+      this.filters = {}
+      return this.getAllDevelopers();
+    },
+
+    onlyNumbers(string) {
+      return string.replace(/[^\d]/, '');
     }
   }
 }
